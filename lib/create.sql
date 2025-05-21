69ccf79711366974f6e3e36d5bc5feabfdaaf670
aminacherop <cheropamina26@gmail.com>
2025-05-21 12:29:59 +0300

    initial commit

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears(
+id INTERGER PRIMARY KEY,
+name TEXT,
+age INTERGER,
+sex TEXT,
+color TEXT,
+temperament  TEXT,
+alive BOOLEAN
+--columns here
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 8, 'M', 'brown', 'calm', 1),
+('Rowdy', 4, 'M', 'black', 'aggressive', 1),
+('Tabitha', 6, 'F', 'blonde', 'friendly', 1),
+('Sergeant Brown', 7, 'M', 'brown', 'strict', 1),
+('Melissa', 5, 'F', 'black', 'shy', 1),
+('Grinch', 9, 'M', 'greenish', 'grumpy', 0),
+('Wendy', 3, 'F', 'white', 'playful', 1),
+(NULL, 2, 'M', 'gray', 'quiet', 1); 
